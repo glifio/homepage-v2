e66c94d2ba0fa82c2d0178db9a231f433c20a25e
Thijs van Hoof <thijs.v.h@gmail.com>
2022-10-31 19:54:49 +0100

    Render test in TestEnvironment

--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -1,7 +1,12 @@
 import { render } from '@testing-library/react'
+import { TestEnvironment } from '@glif/react-components'
 import Home from '@/pages/index'
 
 it('renders the homepage', () => {
-  const { container } = render(<Home />)
+  const { container } = render(
+    <TestEnvironment>
+      <Home />
+    </TestEnvironment>
+  )
   expect(container).toMatchSnapshot()
 })
